29c5833f50da72f31dcf28a0bcb661beb00601a3
Kowalczyk Oliwia <oliwiakowalczyk@vp.pl>
2018-04-12 16:30:40 +0200

    initial commmit

---
+++ Sklep/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sklep
+{
+    public partial class Form1 : Form
+    {
+
+        SklepEntities sklepEntities = new SklepEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DbSet<Drukarki> drukarki = sklepEntities.Drukarki;
+
+
+        }
+    }
+}
